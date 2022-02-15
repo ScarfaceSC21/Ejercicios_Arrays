d41ec357a4f91d43d8f0b6add4975882ac8a5834
UrielTI <urielarmenta05@gmail.com>
2022-02-15 02:54:21 -0800

    Creacion del primer Ejercicio
    Se creo el archivo Ejercicio1.cs que contiene el primer ejercicio del proyecto Base, se actualizo el archivo Progrma.cs, se agrego el metodo Llenado.

---
+++ Ejercicios/Ejercicio1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    /*
+    Realizar un programa que defina un vector llamado “vector_numeros” de 10 enteros, 
+    a continuación lo inicialice con valores aleatorios (del 1 al 10) y posteriormente 
+    muestre en pantalla cada elemento del vector junto con su cuadrado y su cubo.
+    */
+    public class Ejercicio1
+    {
+        public static void Llenado(){
+
+            Random aleatorio = new Random();
+
+            int numero_aleatorio = aleatorio.Next(1,10);
+
+            int[] vector_numeros = new int[10];
+
+            for(int i = 0; i < vector_numeros.Length; i++){
+
+                vector_numeros[i] = numero_aleatorio;
+
+                numero_aleatorio = aleatorio.Next(1,10);
+
+            }
+
+            foreach(int var in vector_numeros){
+
+                System.Console.WriteLine(var);
+
+            }
+
+        }
+    }
+}
